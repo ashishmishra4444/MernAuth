013527abfd77ce9916b1fb36e38c1dc2422e0ca7
Ashish Kumar Mishra <ashishanil334@gmail.com>
2026-01-04 15:17:33 +0530

    created the authRoute file,added the endpoints and verified using postman

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoute.js";
 
 const app = express();
 
@@ -18,9 +19,11 @@ app.use(
   })
 );
 
+//api endpoints..
 app.get("/", (req, res) => {
   res.send("API working");
 });
+app.use('/api/auth',authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
