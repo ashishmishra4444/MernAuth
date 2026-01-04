8526e24e3fc4b06182bd7ec7fdadb4132657bfe6
Ashish Kumar Mishra <ashishanil334@gmail.com>
2026-01-04 12:07:17 +0530

    created the express app

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.get("/",(req,res)=>{
+    res.send("API working");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
