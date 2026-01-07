897002f14150fc8fa1cb9e630753c0abc8a4b963
Ashish Kumar Mishra <ashishanil334@gmail.com>
2026-01-07 14:26:07 +0530

    origin changes done

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-const allowedOrigins = process.env.FRONTEND_URL;
+const allowedOrigins = ['http://localhost:5173']
 
 connectDB();
 
