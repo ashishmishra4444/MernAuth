3bb269b4c71c9f365554fa15e3dee09f282ec6b4
Ashish Kumar Mishra <ashishanil334@gmail.com>
2026-01-07 14:49:23 +0530

    updated the node environment to production, final commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -83,8 +83,10 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
+      // secure: process.env.NODE_ENV === "production",
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     return res.status(200).json({ success: true, message: "Login successful" });
